86a2a0fa34d7258701ffeb73be44a21633580f61
Ondrej Huvar <ohuvar@gmail.com>
2025-11-17 15:06:04 +0100

    [test] Add basic tests with myeloid model.

--- src/tests/inference_naive.rs
+++ src/tests/inference_naive.rs
@@ -7,15 +7,18 @@ use std::fs;
 const TOY_MODEL_4V_PATH: &str = "data/toy_models/4v-activ-fully-spec.aeon";
 const TOY_SPEC_4V_PATH: &str = "data/toy_models/4v-activ-specification.csv";
 
+const MYELOID_BN_PATH: &str = "data/myeloid/myeloid-fully-specified.aeon";
+const MYELOID_DATA_SAT_PATH: &str = "data/myeloid/dataset-fps-adjusted-SAT.csv";
+const MYELOID_DATA_UNSAT_PATH: &str = "data/myeloid/dataset-fps-original-UNSAT.csv";
+
 #[test]
 /// Run the test on a fully specified 4-variable model with activations only.
 /// The model has three fixed points '0000', '0100', '1111'.
-/// The specification requires two fixed points '0110' (fp_1) and '0001' (fp_2)
-/// with confidence weight 0.5 on each bit value.
+/// The specification requires two fixed points '0110' (fp_1) and '0001' (fp_2).
 fn test_toy_model_4v_bn() {
     let bn_string = fs::read_to_string(TOY_MODEL_4V_PATH).unwrap();
     let bn = BooleanNetwork::try_from(bn_string.as_str()).unwrap();
-    let dataset_spec = Dataset::load_dataset(TOY_SPEC_4V_PATH).unwrap();
+    let dataset_spec = Dataset::load_from_csv(TOY_SPEC_4V_PATH).unwrap();
 
     // There is a single optimal specification with 01*0 and 000*
     let mut optimal_solutions = run_naive_inference(&bn, &dataset_spec).unwrap();
@@ -31,4 +34,29 @@ fn test_toy_model_4v_bn() {
     }
     // There are no other optimal specifications
     assert!(optimal_solutions.is_empty())
+}
+
+#[test]
+/// Run the test on a fully specified Myeloid model with two different fixed-point
+/// specifications. The first one is fully satisfiable, while the second requires
+/// a bit flip in the GATA2 value of the Megakaryocyte observation.
+fn test_myeloid_bn() {
+    let bn_string = fs::read_to_string(MYELOID_BN_PATH).unwrap();
+    let bn = BooleanNetwork::try_from(bn_string.as_str()).unwrap();
+    let dataset_spec_sat = Dataset::load_from_csv(MYELOID_DATA_SAT_PATH).unwrap();
+    let dataset_spec_unsat = Dataset::load_from_csv(MYELOID_DATA_UNSAT_PATH).unwrap();
+
+    // The first specification is matched without needing to remove any constraints
+    let mut optimal_solutions = run_naive_inference(&bn, &dataset_spec_sat).unwrap();
+    let expected_removed_constraints = vec![];
+    let solution_set = optimal_solutions.remove(&expected_removed_constraints);
+    assert!(solution_set.is_some());
+    assert!(optimal_solutions.is_empty()); // There are no other optimal specifications
+
+    // The other needs a bit flip in GATA2 of the Megakaryocyte fixed point
+    let mut optimal_solutions = run_naive_inference(&bn, &dataset_spec_unsat).unwrap();
+    let expected_removed_constraints = vec![("Megakaryocyte".to_string(), "GATA2".to_string())];
+    let solution_set = optimal_solutions.remove(&expected_removed_constraints);
+    assert!(solution_set.is_some());
+    assert!(optimal_solutions.is_empty()); // There are no other optimal specifications
 }
--- src/tests/inference_toy_models.rs
+++ src/tests/inference_toy_models.rs
@@ -7,6 +7,10 @@ const TOY_BN_4V_PATH: &str = "data/toy_models/4v-activ-fully-spec.aeon";
 const TOY_PSBN_4V_PATH: &str = "data/toy_models/4v-activ-psbn.aeon";
 const TOY_SPEC_4V_PATH: &str = "data/toy_models/4v-activ-specification.csv";
 
+const MYELOID_BN_PATH: &str = "data/myeloid/myeloid-fully-specified.aeon";
+const MYELOID_DATA_SAT_PATH: &str = "data/myeloid/dataset-fps-adjusted-SAT.csv";
+const MYELOID_DATA_UNSAT_PATH: &str = "data/myeloid/dataset-fps-original-UNSAT.csv";
+
 #[test]
 /// Run the test on a fully specified 4-variable model with activations only.
 /// The model has three fixed points '0000', '0100', '1111'.
@@ -15,7 +19,7 @@ const TOY_SPEC_4V_PATH: &str = "data/toy_models/4v-activ-specification.csv";
 fn test_toy_bn_4v_bn() {
     let bn_string = fs::read_to_string(TOY_BN_4V_PATH).unwrap();
     let bn = BooleanNetwork::try_from(bn_string.as_str()).unwrap();
-    let dataset_spec = Dataset::load_dataset(TOY_SPEC_4V_PATH).unwrap();
+    let dataset_spec = Dataset::load_from_csv(TOY_SPEC_4V_PATH).unwrap();
 
     let inference_problem = dataset_spec.to_inference_problem(&bn).unwrap();
     let fix_one = inference_problem.get_state("fp_1");
@@ -47,7 +51,7 @@ fn test_toy_psbn_4v_bn() {
     let bn_string = fs::read_to_string(TOY_PSBN_4V_PATH).unwrap();
     let bn = BooleanNetwork::try_from(bn_string.as_str()).unwrap();
     let f = ParameterId::from_index(0);
-    let dataset_spec = Dataset::load_dataset(TOY_SPEC_4V_PATH).unwrap();
+    let dataset_spec = Dataset::load_from_csv(TOY_SPEC_4V_PATH).unwrap();
 
     let inference_problem = dataset_spec.to_inference_problem(&bn).unwrap();
     let fix_one = inference_problem.get_state("fp_1");
@@ -72,4 +76,68 @@ fn test_toy_psbn_4v_bn() {
     assert_eq!(bdd_fn, expected);
 
     // TODO: check for the second sat model
+}
+
+#[test]
+/// Run the inference on a fully specified Myeloid model.
+/// For this test, we use a specification that requires four different fixed points which
+/// are all directly satisfied.
+fn test_myeloid_bn_sat() {
+    let bn_string = fs::read_to_string(MYELOID_BN_PATH).unwrap();
+    let bn = BooleanNetwork::try_from(bn_string.as_str()).unwrap();
+    let dataset_spec = Dataset::load_from_csv(MYELOID_DATA_SAT_PATH).unwrap();
+
+    let inference_problem = dataset_spec.to_inference_problem(&bn).unwrap();
+
+    // Result should be SAT, the model completely satisfying the specification
+    let solver = inference_problem.build_solver();
+    assert_eq!(solver.check(&[]), SatResult::Sat);
+
+    let model = solver.get_model().unwrap();
+    for (obs_id, obs) in dataset_spec.observations {
+        let fix_state = inference_problem.get_state(&obs_id);
+        // All required fixpoint states match the original specification directly
+        assert_eq!(
+            fix_state.extract_state(&model),
+            obs.value_map.values().cloned().collect::<Vec<bool>>()
+        );
+    }
+}
+
+#[test]
+/// Run the inference on a fully specified Myeloid model.
+/// For this test, we use a specification that requires four different fixed points,
+/// and one of the values needs to be flipped for it to be satisfied.
+fn test_myeloid_bn_unsat() {
+    let bn_string = fs::read_to_string(MYELOID_BN_PATH).unwrap();
+    let bn = BooleanNetwork::try_from(bn_string.as_str()).unwrap();
+    let dataset_spec = Dataset::load_from_csv(MYELOID_DATA_UNSAT_PATH).unwrap();
+
+    let inference_problem = dataset_spec.to_inference_problem(&bn).unwrap();
+
+    // Result should be SAT, the model differing in one bit from the specification
+    // The different bit is GATA2 value (first value) in Megakaryocyte observation
+    let solver = inference_problem.build_solver();
+    assert_eq!(solver.check(&[]), SatResult::Sat);
+
+    // Find index of GATA2 (variables in SMT state are alphabetical)
+    let mut variables = dataset_spec.variables.clone();
+    variables.sort();
+    let gata_idx = variables.iter().position(|v| v == "GATA2").unwrap();
+
+    let model = solver.get_model().unwrap();
+    for (obs_id, obs) in dataset_spec.observations {
+        let fix_state = inference_problem.get_state(&obs_id);
+        let mut fix_orig_spec = obs.value_map.values().cloned().collect::<Vec<bool>>();
+        if obs_id.as_str() == "Megakaryocyte" {
+            // Megakaryocyte fixpoint in the model does not match the original specification
+            assert_ne!(fix_state.extract_state(&model), fix_orig_spec);
+            // But if we swap the GATA2 to false, it matches
+            fix_orig_spec[gata_idx] = false;
+            assert_eq!(fix_state.extract_state(&model), fix_orig_spec);
+        } else {
+            // All other required fixpoint states match the original specification directly
+            assert_eq!(fix_state.extract_state(&model), fix_orig_spec);
+        }
+    }
 }
