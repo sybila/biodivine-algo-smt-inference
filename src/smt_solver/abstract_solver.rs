f57b6063c5a27d12a8042cda4c37824686131188
Samuel Pastva <sam.pastva@gmail.com>
2026-02-26 20:39:48 +0100

    feat: Function for extracting point-based function from an `AbstractSolver`.

--- src/smt_solver/abstract_solver.rs
+++ src/smt_solver/abstract_solver.rs
@@ -1,7 +1,12 @@
+use crate::smt_solver::{
+    IntAtom, IntFunction, extract_function_applications, extract_function_type_signature,
+    model_eval_int,
+};
 use auto_impl::auto_impl;
 use log::trace;
-use z3::ast::Bool;
-use z3::{Model, SatResult};
+use std::collections::BTreeMap;
+use z3::ast::{Ast, Bool};
+use z3::{FuncDecl, Model, SatResult};
 
 /// The most basic trait implemented by all solvers.
 #[auto_impl(Box)]
@@ -10,6 +15,38 @@ pub trait AbstractSolver {
     fn check(&self) -> SatResult;
     fn get_model(&self) -> Option<Model>;
     fn get_assertions(&self) -> Vec<Bool>;
+
+    /// Identify all points (input combinations) that are exactly determined by the current
+    /// solver query in the provided model and place them into an [`IntFunction`].
+    fn extract_function_points(
+        &self,
+        f: &FuncDecl,
+        model: &Model,
+    ) -> Result<IntFunction, anyhow::Error> {
+        let signature = extract_function_type_signature(f)?;
+        let mut terms: BTreeMap<u32, Vec<Vec<IntAtom>>> = BTreeMap::new();
+
+        for assertion in self.get_assertions() {
+            for func_call in extract_function_applications(&assertion) {
+                if func_call.decl().name() != f.name() {
+                    continue;
+                }
+                let args = func_call
+                    .children()
+                    .iter()
+                    .enumerate()
+                    .map(|(i, child)| IntAtom::eq(i, model_eval_int(child, model)))
+                    .collect::<Vec<_>>();
+
+                let output = model_eval_int(&func_call, model);
+                terms.entry(output).or_default().push(args);
+            }
+        }
+
+        let mut function = IntFunction { signature, terms };
+        function.remove_duplicates();
+        Ok(function)
+    }
 }
 
 impl AbstractSolver for z3::Solver {
